93ed4f2d8839dc4ad41b50cb02a8e652e734a49e
Jani Mäkelä <janimakelaa@gmail.com>
2022-08-17 20:43:25 +0300

    Add setup shell script that symlinks all the necessary files

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script setups all the symlinks that are necessary
+
+# Absolute path to this script, e.g. /home/user/dotfiles
+BASEDIR=$(dirname $(readlink -f $0))
+
+ln -sf $BASEDIR/.bashrc $HOME/.bashrc
+ln -sf $BASEDIR/.bash_profile $HOME/.bash_profile
