574c527365ad4076a0b2dd8ca4fd0666f219a25a
Jani Mäkelä <janimakelaa@gmail.com>
2022-08-21 20:43:59 +0300

    Add .gitconfig to dotfiles

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ BASEDIR=$(dirname $(readlink -f $0))
 
 ln -sf $BASEDIR/.bashrc $HOME/.bashrc
 ln -sf $BASEDIR/.bash_profile $HOME/.bash_profile
+ln -sf $BASEDIR/.gitconfig $HOME/.gitconfig
 ln -sf $BASEDIR/.spacemacs $HOME/.spacemacs
