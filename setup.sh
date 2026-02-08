19b5c35ed506bfde0e6ba5c561a502e1c94096d1
Jani Mäkelä <janimakelaa@gmail.com>
2026-02-08 17:22:35 +0200

    feat: Setup pgp to read passkey from keychain

--- setup.sh
+++ setup.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-# Setup homebrew, make this check if it is installed first
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Setup homebrew, check if it is installed first
+if ! command -v brew &> /dev/null; then
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo >> /Users/jmakela/.zprofile
-echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"' >> /Users/jmakela/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv zsh)"
+  echo >> /Users/jmakela/.zprofile
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"' >> /Users/jmakela/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv zsh)"
+fi
 
 brew install git
 brew install gnupg pinentry-mac
 
-# Ask user to setup SSH keys with github, link to documentation
-# https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
-
-# Ask user to setup GPG keys with github
-
 BASEDIR=$(dirname $(readlink -f $0))
 
-ln -sf $BASEDIR/.zshrc $HOME/.zshrc
-ln -sf $BASEDIR/.zprofile $HOME/.zprofile
-ln -sf $BASEDIR/.gitconfig $HOME/.gitconfig
+# Create symlinks
+$BASEDIR/link.sh
 
 brew bundle
